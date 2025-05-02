f3bb85dc8de31c60cf47c4f55d8ff7f75bf16945
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-05-02 04:25:27 +0300

    added my first project

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import "./Card.css"; // Import the CSS file for styling
 const Card = ({ header, image, description , link1 , link2}) => {
   return (
     <div className="cardd">
-      <h2 className="text-xl font-bold mb-2 text-gray-900 dark:text-white">{header}</h2>
+      <h2 className="projheader text-xl font-bold mb-2 text-gray-900 dark:text-white">{header}</h2>
       <img src={image} alt={header} className="projimg" />
       <p className="text-gray-700 dark:text-gray-300">{description}</p>
     <div className="card-button-container">
